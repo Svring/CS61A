dbf1a9e0730f7a89e110a175719389f568730123
Svring <lxf83553899@163.com>
2022-07-21 12:25:48 +0800

    lab12

---
+++ lab/lab12/1.scm
@@ -0,0 +1,9 @@
+(define (three n) 
+  (cons-stream n (three (+ n 3)))
+)
+
+(define f (three 0))
+
+(car f)
+
+(car (cdr-stream f))
---
+++ lab/lab12/lab12.scm
@@ -0,0 +1,32 @@
+
+(define-macro (def func args body)
+  `(define ,(cons func args) ,body)
+)
+
+
+(define (map-stream f s)
+    (if (null? s)
+    	nil
+    	(cons-stream (f (car s)) (map-stream f (cdr-stream s)))))
+
+(define all-three-multiples
+  (map-stream (lambda (x) (+ x 3)) (cons-stream 0 all-three-multiples))
+)
+
+
+(define (compose-all funcs)
+  (if (null? funcs) (lambda (x) x)
+  (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x)))
+  )
+)
+
+
+(define (partial-sums stream)
+  (define (helper start s)
+    (if (null? s) nil
+    (cons-stream (+ (car s) start) (helper (+ (car s) start) (cdr-stream s)))
+    )
+  )
+  (helper 0 stream)
+)
+
